ec5e213ff637934014feadc517d963de03ea5b52
ARTURO_ <pedroarturocampos@gmail.com>
2022-11-04 09:45:34 -0600

    Cambios
    Se agrego el ejercicio de la animacion con GreenSock

---
+++ js/animacion.js
@@ -0,0 +1,9 @@
+function Inicio() {
+    gsap.to(".person", { x: 730, scale: 1, rotation: 0, duration: 10, repeat: -1 });
+}
+
+// function tiempo1() {
+//     setTimeout(() => primera(), 1000);
+// }
+
+Inicio()
